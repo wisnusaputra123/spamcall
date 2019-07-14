78db8a27a1ab4ea33d521fc1bd9a57cc7dae4ba3
wisnusaputra123 <52888512+wisnusaputra123@users.noreply.github.com>
2019-07-15 03:41:42 +0700

    Update call.php

--- call.php
+++ call.php
@@ -1,23 +1,12 @@
-#!/bin/bash
-#version 1.0
+##############################################
 
-# Variables
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
+  >>>> | Author    = wisnu saputra.| <<<<
+       |                           |
+  >>>> | Instagram = wisnu_as123   | <<<<
 
-figlet Wisnu Tools | lolcat
+
+      "Gunakan script dengan bijak"
+##############################################
 
 <?php
 // Limit 3x Telpon Setiap Satu Nomor
