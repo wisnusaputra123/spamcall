5ffb3358d2dfc7ca70f2eff1697362d48919deb0
wisnusaputra123 <52888512+wisnusaputra123@users.noreply.github.com>
2019-07-15 02:58:08 +0700

    Update and rename spam.php to call.php

--- call.php
+++ call.php
@@ -10,9 +10,7 @@ function send($phone){
         curl_setopt($ch, CURLOPT_POSTFIELDS, "msisdn=$phone&accept=call");                        $asw = curl_exec($ch);
         curl_close($ch);
                 echo $asw."\n";
-}
-figlet wisnu
- instagram =wisnu_as123       
+}    
 echo "AUTHOR ; WisnuSaputra\n\n";
 echo "Nomor\nInput : ";
 $nomor = trim(fgets(STDIN));
