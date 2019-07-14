3f37bcb63380fa1a62669348a75a37cf600b03c4
wisnusaputra123 <52888512+wisnusaputra123@users.noreply.github.com>
2019-07-15 03:32:48 +0700

    Update call.php

--- call.php
+++ call.php
@@ -1,6 +1,23 @@
-cowsay -f eyes My Tools | lolcat
-figlet wisnu saputra  | lolcat
-toilet -f standard welcome -F gay
+#!/bin/bash
+#version 1.0
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet Wisnu Tools | lolcat
 
 <?php
 // Limit 3x Telpon Setiap Satu Nomor
